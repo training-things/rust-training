4aee5d065c4ab576f3f93e61b5edc9ab2f366e9b
aierui <aieruishi@gmail.com>
2023-01-04 22:39:36 +0800

    chore: learn futrues block_on

---
+++ src/async/futures-case2/src/main.rs
@@ -0,0 +1,56 @@
+// `block_on` blocks the current thread until the provided future has run to
+// completion. Other executors provide more complex behavior, like scheduling
+// multiple futures onto the same thread.
+use std::{thread, time::Duration};
+
+use futures::executor::block_on;
+
+struct Song {
+    author: String,
+    name: String,
+}
+
+async fn learn_song() -> Song {
+    Song {
+        author: "周杰伦".to_string(),
+        name: String::from("《菊花台》"),
+    }
+}
+
+async fn sing_song(song: Song) {
+    println!(
+        "给大家献上一首{}的{} ~ {}",
+        song.author, song.name, "菊花残，满地伤~ ~"
+    );
+}
+
+async fn dance() {
+    println!("唱到情深处，身体不由自主的动了起来~ ~");
+}
+
+async fn learn_and_sing() {
+    // Wait until the song has been learned before singing it.
+    // We use `.await` here rather than `block_on` to prevent blocking the
+    // thread, which makes it possible to `dance` at the same time.
+    let song = learn_song().await;
+
+    thread::sleep(Duration::from_millis(4000));
+
+    sing_song(song).await;
+}
+
+async fn async_main() {
+    let f1 = learn_and_sing();
+    let f2 = dance();
+
+    // `join!` is like `.await` but can wait for multiple futures concurrently.
+    // If we're temporarily blocked in the `learn_and_sing` future, the `dance`
+    // future will take over the current thread. If `dance` becomes blocked,
+    // `learn_and_sing` can take back over. If both futures are blocked, then
+    // `async_main` is blocked and will yield to the executor.
+    futures::join!(f1, f2);
+}
+
+fn main() {
+    block_on(async_main());
+}
